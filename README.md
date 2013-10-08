20f0224340090ae493f3d6bc7d27abde68e68b4e
Paul Sweeney <paul@8-bitdesign.com>
2013-10-08 00:17:23 -0700

    Revised API

---
+++ README.md
@@ -0,0 +1,106 @@
+# What is this?
+
+A NodeJS library to talk to Target Process 3's API.
+
+## How do I use it?
+
+In your NodeJS program, add:
+
+``` javascript
+var tp = require('tp-api')({
+           domain:   // your domain here; eg: 'fullscreen.tpondemand.com'
+           username: // your username; eg: 'paul@fullscreen.net'
+           password: // your password
+         })
+
+tp('Tasks')
+  .take(5)
+  .where("EntityState.Name eq 'Open'")
+  .pluck('Name', 'NumericPriority')
+  .sortBy('NumericPriority')
+  .then(function(err, tasks) {
+    console.log('my tasks', tasks)
+  }
+)
+```
+
+### Methods
+
+#### `tp()` or `tp([Entity name])`
+Returns a TargetProcess request object. Can optionally set an entity to request.
+
+#### `tp.get(Entity Name)`
+Tells the TargetProcess object to request the given entity. An entity can be one
+of the following:
+
+* Projects
+* Features
+* Releases
+* Iterations
+* Requests
+* CustomFields
+* Bugs
+* Tasks
+* TestCases
+* Times
+* Impediments
+* Assignments
+* Attachments
+* Comments
+* UserStories
+* Roles
+* GeneralUsers
+
+Example - fetch a list of roles
+``` javascript
+tp('Roles').then(function(err, tasks) { ... })
+```
+
+#### `tp.take(n)`
+Tells the TargetProcess object to request `n` of the requested entities
+
+Example - fetch 5 tasks:
+``` javascript
+tp('Tasks').take(5).then(function(err, tasks) { ... })
+```
+
+#### `tp.where(search expression)`
+Applies a seach filter to the TargetProcess request.
+
+Example, find open tasks:
+
+``` javascript
+tp('Tasks').where("EntityState.Name eq 'Open'").then(function(err, tasks) { ... })
+```
+
+More info on Target Process filters can be found here:
+http://dev.targetprocess.com/rest/response_format#filtering
+
+#### `tp.pluck(list, of, properties)`
+Instructs the Target Process object to only include the listed properties in the
+response object:
+
+Example, fetch the name and description from these tasks:
+``` javascript
+tp('Tasks').pluck('Name', 'Description').then(function(err, tasks) { ... })
+```
+
+#### `tp.sortBy(field)`
+Tells the Target Process request to sort on the given field
+
+Example, fetch a list of tasks by priority:
+``` javascript
+tp('Tasks').sortBy('NumericPriority').then(function(err, tasks) { ... })
+```
+
+#### `tp.then(handlerFunction)`
+Executes the Target Process request. Your callback will be called with an
+error object and the result of your request.
+
+Example, fetch a list of tasks:
+``` javascript
+tp('Tasks').then(function(err, tasks) {
+  console.log('My tasks', tasks)
+  console.error('Errors from the request', err)
+})
+```
--- index.js
+++ index.js
@@ -1,58 +1,79 @@
 var request = require('request')
 
-function toArgs(args) {
-  return Array.prototype.slice.call(args)
-}
+function configure(opts) {
+  // Catch folks using the `new` keyword when invoking our configurator
+  if (this instanceof configure) { return configure(opts) }
 
-function TP(opts) {
-  opts = opts || {}
-
-  if (!opts.domain || !opts.token) {
-    throw new Error('Token and domain are required')
+  if (!opts.token && opts.username && opts.password) {
+    opts.token = new Buffer(opts.username+':'+opts.password).toString('base64')
+  } else if (!opts.token) {
+    throw new Error('A TargetProcess username and password is required')
   }
 
-  this.version = opts.version || 1 
-  this.domain = opts.domain
-  this.token = opts.token
-  this.urlRoot = 'https://'+this.domain+'/api/v'+this.version
-}
+  if (!opts.domain) {
+    throw new Error('A TargetProcess domain is required')
+  }
 
-TP.prototype.handle = function(cb) {
-  return function(err, res, json) {
-    if (json.Items) { json = json.Items }
-    cb(err, json)
+  var version = opts.version || 1
+  var domain = opts.domain
+  var token = opts.token
+  var urlRoot = 'https://'+domain+'/api/v'+version
+
+  return function(entity) {
+    var instance = new TPRequest(urlRoot, token);
+    if (entity) { instance.get(entity) }
+    return instance
   }
 }
 
-TP.prototype.get = function(/* entity, [opts, ] callback */) {
-  var args = toArgs(arguments)
-    , cb = args.pop()
-    , entity = args.shift()
-    , qs = args.pop() || {}
-
-  var opts = {
-    url: this.urlRoot +'/'+ entity,
+function TPRequest(baseUrl, token) {
+  this.baseUrl = baseUrl
+  this.opts = {
     json: true,
-    qs: qs,
-    headers: { Authorization: 'Basic '+ this.token }
+    qs: {},
+    headers: { Authorization: 'Basic '+ token }
   }
-  
-  request(opts, this.handle(cb))
 }
 
-
-var entities = [ 'Projects', 'Features', 'Releases', 'Iterations', 'Requests',
-                 'CustomFields', 'Bugs', 'Tasks', 'TestCases', 'Times',
-                 'Impediments', 'Assignments', 'Attachments', 'Comments',
-                 'UserStories', 'Roles', 'GeneralUsers'
+TPRequest.prototype.entities = [
+  'Projects', 'Features', 'Releases', 'Iterations', 'Requests',
+  'CustomFields', 'Bugs', 'Tasks', 'TestCases', 'Times',
+  'Impediments', 'Assignments', 'Attachments', 'Comments',
+  'UserStories', 'Roles', 'GeneralUsers'
 ]
 
-entities.forEach(function(entity) {
-  TP.prototype[entity.toLowerCase()] = function() {
-    var args = toArgs(arguments)
-    args.unshift(entity)
-    this.get.apply(this, args)
-  }
-})
+TPRequest.prototype.get = function(entity) {
+  this.opts.url = this.baseUrl+'/'+entity
+  return this
+}
 
-module.exports = TP
+TPRequest.prototype.take = function(number) {
+  this.opts.qs.take = number
+  return this
+}
+
+TPRequest.prototype.where = function(search) {
+  this.opts.qs.where = search
+  return this
+}
+
+TPRequest.prototype.pluck = function() {
+  var args = Array.prototype.slice.call(arguments)
+
+  this.opts.qs.include = '[' + args.join(',') + ']'
+  return this
+}
+
+TPRequest.prototype.sortBy = function(property) {
+  this.opts.qs.orderBy = property
+  return this
+}
+
+TPRequest.prototype.then = function(cb) {
+  request(this.opts, function(err, res, json) {
+    if (json && json.Items) { json = json.Items }
+    cb(err, json)
+  })
+}
+
+module.exports = configure
