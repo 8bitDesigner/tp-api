1902eceee20870c7b714d26902d1f83d1ec86e34
Paul Sweeney <paul@8-bitdesign.com>
2013-10-09 11:16:36 -0700

    Error checking

--- index.js
+++ index.js
@@ -79,9 +79,24 @@ TPRequest.prototype.sortBy = function(property) {
 }
 
 TPRequest.prototype.then = function(cb) {
+  var opts = this.opts
+
   request(this.opts, function(err, res, json) {
     if (json && json.Items) { json = json.Items }
-    cb(err, json)
+
+    if (typeof json === 'string') {
+      err = new Error("Couldn't find resorce at "+ opts.url)
+    }
+
+    if (json.Error) {
+      err = new Error(json.Error.Message)
+    }
+
+    if (typeof err === 'string') {
+      err = new Error(err)
+    }
+
+    cb(err, (err) ? null : json)
   })
 }
 
