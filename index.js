2d0bcf3bc09b3d34ea5494d9f5fcdffb7436c467
Julian Kleinhans <julian.kleinhans@aoe.com>
2016-01-14 08:51:38 +0100

    Add possibility to add an ACID to retrieve a list of entities for specific projects

--- index.js
+++ index.js
@@ -16,15 +16,19 @@ function configure(opts) {
   if (!opts.domain) {
     throw new Error('A TargetProcess domain is required')
   }
+  if (!opts.acid) {
+    opts.acid = null
+  }
 
   var version = opts.version || 1
   var domain = opts.domain
   var token = opts.token
+  var acid = opts.acid
   var protocol = opts.protocol || 'https'
   var urlRoot = protocol + '://'+domain+'/api/v'+version
 
   return function(entity, id) {
-    var collection = new TPQuery(urlRoot, token)
+    var collection = new TPQuery(urlRoot, token, acid)
       , model
 
     if (entity) { collection.get(entity) }
